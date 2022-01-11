bcf5a52ec2eba311a3ef8a9b983e88d5deeb4206
Tung Huynh <43397711+codyhuynh-htt@users.noreply.github.com>
2022-01-12 06:00:39 +0700

    First Commit

---
+++ src/test/java/fpt/greenwich/ppmtool/PpmToolFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.greenwich.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
